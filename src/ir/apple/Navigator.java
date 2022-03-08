4b350c56f85afc6dadabb368a1bf39b67540a180
hp <abbasabbaspour313@gmail.com>
2022-03-09 00:04:47 +0330

    Initial commit

---
+++ src/ir/apple/Navigator.java
@@ -0,0 +1,12 @@
+package ir.apple;
+
+/**
+ * this class navigate us to the target classes
+ */
+public class Navigator {
+
+
+    public static void main(String args []) {
+      System.out.println("Navigator");
+    }
+}
